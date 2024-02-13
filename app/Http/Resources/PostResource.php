bf3e7c3e3ccc6f33a5ee7c04dcb3608c4591a44e
miiiwdy <fahmiwidyaa@gmail.com>
2024-02-13 11:12:54 +0700

    latihan restapi-laravel

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    //defprop
+    public $status;
+    public $message;
+    public $resource;
+
+    /**
+    * __construct
+    *
+    * @param mixed $status
+    * @param mixed $message
+    * @param mixed $resource
+    * @return void
+    */
+
+    public function __construct($status, $message, $resource) {
+        parent::__construct($resource);
+        $this->status = $message;
+        $this->message = $message;
+    }
+    /**
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'succes' => $this->status,
+            'message' => $this->message,
+            'data' => $this->resource
+        ];
+    }
+}
